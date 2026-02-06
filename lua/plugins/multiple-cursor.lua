321db656c2e61caf32912930e9a431c452c2d67c
Ocheretin Daniil <daniilocheretin99@gmail.com>
2026-02-07 00:13:28 +0300

    update (#1)
    Co-authored-by: memento mori <Daniil.Ocheretin@lemanapro.ru>

--- lua/plugins/multiple-cursor.lua
+++ lua/plugins/multiple-cursor.lua
@@ -1,11 +1,10 @@
 return {
 	"brenton-leighton/multiple-cursors.nvim",
-	version = "*", -- Use the latest tagged version
+	virsion = "*", -- Use the latest tagged version
 	opts = {}, -- This causes the plugin setup function to be called
 	keys = {
 		-- { "<C-J>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
 		-- { "<C-K>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
-
 		{ "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
 		{ "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
 	},
